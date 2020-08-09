b5e5be88df487ddfd077058e292a2b0d3efe6efc
macbook <macbook@AIs-MacBook-Pro.local>
2020-08-09 18:40:53 +0600

    upd

--- Training/B.Бал_в_БерлГУ.cpp
+++ Training/B.Бал_в_БерлГУ.cpp
@@ -18,7 +18,37 @@ void print(vector<int> &a){
 }
 
 void test_case(){
+    int n;
+    cin >> n;
 
+    vector<int> a(n);
+
+    for(int i=0; i < n; ++i) cin >> a[i];
+
+    int m;
+    cin >> m;
+
+    vector<int> b(m);
+
+    for(int i=0; i < m; ++i) cin >> b[i];
+
+    vector<bool> used(m,false);
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int cnt = 0;
+
+    for(int i=0; i < n; ++i){
+        for(int j=0; j < m; ++j){
+            if(!used[j] && abs(a[i]-b[j])<=1){
+                used[j] = true;
+                cnt++;
+                break;
+            }
+        }
+    }
+    cout << cnt << endl;
 }
 
 int main(){
