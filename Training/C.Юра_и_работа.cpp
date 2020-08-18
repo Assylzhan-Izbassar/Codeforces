69b72d62a764b860b57c2f494a79bb577812c652
macbook <macbook@AIs-MacBook-Pro.local>
2020-08-18 15:21:05 +0600

    upd

--- Training/C.Юра_и_работа.cpp
+++ Training/C.Юра_и_работа.cpp
@@ -30,6 +30,7 @@ void print(vector<int> &a){
     }
     cout << endl;
 }
+
 void print(vector<ll> &a){
     for(int i=0; i < a.size(); ++i){
         cout << a[i] << " ";
@@ -38,7 +39,38 @@ void print(vector<ll> &a){
 }
 
 void test_case(){
+    int n, m, k;
+    cin >> n >> m >> k;
 
+    vector<int> p(n);
+    vector<ll> psum(n+1);
+
+    for(int i=1; i <=n; ++i) cin >> p[i-1], psum[i] = psum[i-1] + p[i-1];
+
+    ll dp[k+1][n+1];
+
+    for(int i=0; i <= k; ++i)
+        for(int j=0; j <= n; ++j)
+            dp[i][j] = 0;
+
+    for(int i=1; i <= k; ++i){
+        for(int j=1; j <= n; ++j){
+            if(i*m > j){
+                dp[i][j] = 0;
+            }else{
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j-m] + psum[j] - psum[j-m]);
+            }
+        }
+    }
+    // print(psum);
+
+    //  for(int i=0; i <= k; ++i){
+    //     for(int j=0; j <= n; ++j){
+    //         cout << dp[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+    cout << dp[k][n] << endl;
 }
 
 int main(){
